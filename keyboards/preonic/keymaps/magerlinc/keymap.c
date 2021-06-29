ef56eacca45d596e8ebd940dc2a485d7f7b8fe52
mikkel agerlin <mikkel_ac@hotmail.com>
2021-06-29 14:48:21 +0200

    Further nordic vowel tests

--- keyboards/preonic/keymaps/magerlinc/keymap.c
+++ keyboards/preonic/keymaps/magerlinc/keymap.c
@@ -32,7 +32,6 @@ void keyboard_post_init_user(void) {
 
 // Defines the keycodes used by our macros in process_record_user
 enum custom_keycodes {
-// Demo code
   DK_AE,
   DK_OE,
   DK_AA,
@@ -191,7 +190,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_RAISE] = LAYOUT_preonic_grid(
   MY_TICK,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    DK_OE,    DK_AA,
-  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    DK_AE,    DK_OE,
+  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    DK_AE,    KC_DEL,
   KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,
   _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_NUHS, KC_NUBS, KC_PGUP, KC_PGDN, _______,
   _______, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY
@@ -296,11 +295,13 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
           break;
         case DK_AE:
           if (record->event.pressed) {
-            SEND_ALT_CODE("0230", "0198");
+            //SEND_ALT_CODE("0230", "0198");
+            SEND_ALT_CODE("0248", "0216");
           }
           break;
         case DK_OE:
-          if (record->event.pressed) {
+         if (record->event.pressed) {
+            //SEND_ALT_CODE("0230", "0198");
             SEND_ALT_CODE("0248", "0216");
           }
           break;
